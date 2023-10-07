86b59ab2b529b3a2c076a0e27aba680f5eb187ca
Odissey7 <agapidu@inbox.ru>
2023-10-07 16:07:55 +0300

    Доделывает сайт

---
+++ JS/index.js
@@ -0,0 +1,14 @@
+const huga = document.querySelector(`.my__name`);
+const huga_team = document.querySelector(`.about_my-team-none`);
+
+huga.onclick = function (evt) {
+  evt.preventDefault();
+  setTimeout(() => {
+    huga_team.classList.toggle(`about_my-team`);
+    document.onkeydown = function (evt) {
+      if (evt.key === `Escape` || this.onclick) {
+          huga_team.classList.remove(`about_my-team`);
+      }
+    };
+  }, 300)
+}
